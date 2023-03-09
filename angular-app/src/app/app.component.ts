7a1f907717a3d7e32cb31cd5b17813eb9d1d962d
Anupriya Batra <anupriya@backbase.com>
2023-03-10 00:41:19 +0100

    feat:add function

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { map } from 'rxjs/operators';
+
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+  user$ : any;
+
+  ngOnInit() {
+  this.user$ = this.http.get('/api/users').pipe(
+      map((response: any) => response)
+    );
+  }
+  
+  constructor(
+    private readonly http: HttpClient,
+  ) {}
+  
 }
+
--- angular-app/src/environments/environment.prod.ts
+++ angular-app/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_ROOT: 'https://nice-hill-0e9a09303.2.azurestaticapps.net',
 };
